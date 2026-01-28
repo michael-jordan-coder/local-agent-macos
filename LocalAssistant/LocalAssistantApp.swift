0fcf35e63058d3cae35819bcdb3383e9da0feec4
npmDada <daniel.gourarye@gmail.com>
2026-01-28 23:37:38 +0200

    Initial Commit

---
+++ LocalAssistant/LocalAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAssistantApp.swift
+//  LocalAssistant
+//
+//  Created by gur arye on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
